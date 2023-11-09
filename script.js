652092eeb7f15e6e4c998e1899959fa5b9d2d729
Anthony Ngo <anthony.g.ngo@gmail.com>
2023-11-08 20:55:53 -0800

    add scoring logic and board clearing

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 const playerScore = document.getElementById('player-score');
 const computerScore = document.getElementById('computer-score');
 const playerChoice = document.getElementById('player-choice');
+const computerChoice = document.getElementById('computer-choice');
 const playerRock = document.getElementById('player-rock');
 const playerPaper = document.getElementById('player-paper');
 const playerScissors = document.getElementById('player-scissors');
+const vs = document.getElementById('vs');
+
+let currentPlayerScore = 0;
+let currentComputerScore = 0;
 
 const getComputerChoice = () => {
 	const randomChoice = Math.floor(Math.random() * 3);
 	switch (randomChoice) {
 		case 0:
+			computerChoice.setAttribute('src', './assets/rock.png');
 			return 'rock';
 		case 1:
+			computerChoice.setAttribute('src', './assets/paper.png');
 			return 'paper';
 		case 2:
+			computerChoice.setAttribute('src', './assets/scissors.png');
 			return 'scissors';
 		default:
 			throw new Error('Something went wrong with the finding a random int');
@@ -50,25 +58,46 @@ const round = (playerSelection, gameSelection) => {
 	}
 };
 
+const clearBoard = () => {
+	playerChoice.setAttribute('src', '');
+	computerChoice.setAttribute('src', '');
+	vs.textContent = 'VS.';
+};
+
 const game = () => {
-	let currentPlayerScore = 0;
-	let currentComputerScore = 0;
+	const addScore = (results) => {
+		if (results === 0) {
+			vs.textContent = `Draw`;
+			return;
+		} else if (results === 1) {
+			currentPlayerScore++;
+			playerScore.textContent = `You: ${currentPlayerScore}`;
+			vs.textContent = `You win`;
+		} else {
+			currentComputerScore++;
+			computerScore.textContent = `Computer: ${currentComputerScore}`;
+			vs.textContent = `Computer wins`;
+		}
+	};
 
 	playerRock.addEventListener('click', () => {
 		playerChoice.setAttribute('src', './assets/rock.png');
 		let results = round('rock', getComputerChoice());
-		console.log(results);
+		addScore(results);
+		setTimeout(clearBoard, 2000);
 	});
 
 	playerPaper.addEventListener('click', () => {
 		playerChoice.setAttribute('src', './assets/paper.png');
 		let results = round('paper', getComputerChoice());
+		addScore(results);
+		setTimeout(clearBoard, 2000);
 	});
 
 	playerScissors.addEventListener('click', () => {
 		playerChoice.setAttribute('src', './assets/scissors.png');
 		let results = round('scissors', getComputerChoice());
+		addScore(results);
+		setTimeout(clearBoard, 2000);
 	});
 };
-
-game();
