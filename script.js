fe5f5f77b7012dfa170550185afaeac91aba959e
Anthony Ngo <anthony.g.ngo@gmail.com>
2023-11-09 12:30:41 -0800

    add game win logic

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const playerScore = document.getElementById('player-score');
 const computerScore = document.getElementById('computer-score');
 const playerChoice = document.getElementById('player-choice');
+const playerOptions = document.getElementById('player-options');
 const computerChoice = document.getElementById('computer-choice');
+const computerOptions = document.getElementById('computer-options');
 const playerRock = document.getElementById('player-rock');
 const playerPaper = document.getElementById('player-paper');
 const playerScissors = document.getElementById('player-scissors');
 const vs = document.getElementById('vs');
+const winner = document.getElementById('winner');
 
 let currentPlayerScore = 0;
 let currentComputerScore = 0;
+
+let gameOver = false;
 
 const getComputerChoice = () => {
 	const randomChoice = Math.floor(Math.random() * 3);
@@ -73,28 +78,43 @@ const addScore = (results) => {
 	} else if (results === 1) {
 		currentPlayerScore++;
 		playerScore.textContent = `You: ${currentPlayerScore}`;
-		vs.textContent = `You win`;
+		// vs.textContent = `You win`;
 		return;
 	} else {
 		currentComputerScore++;
 		computerScore.textContent = `Computer: ${currentComputerScore}`;
-		vs.textContent = `Computer wins`;
+		// vs.textContent = `Computer wins`;
 		return;
 	}
 };
 
-const playerChoiceListener = () => {
-	playerChoice.setAttribute('src', './assets/rock.png');
-	let results = round('rock', getComputerChoice());
-	addScore(results);
-	setTimeout(clearBoard, 2000);
+const checkWin = () => {
+	if (currentPlayerScore >= 3 || currentComputerScore >= 3) {
+		playerOptions.classList.toggle('none');
+		computerOptions.classList.toggle('none');
+		vs.classList.toggle('none');
+		winner.classList.toggle('none');
+		if (currentPlayerScore > currentComputerScore) {
+			winner.textContent = `Game Over - Player Wins!`;
+		} else {
+			winner.textContent = `Game Over - Computer Wins`;
+		}
+	}
 };
+
+// const playerChoiceListener = () => {
+// 	playerChoice.setAttribute('src', './assets/rock.png');
+// 	let results = round('rock', getComputerChoice());
+// 	addScore(results);
+// 	setTimeout(clearBoard, 2000);
+// };
 
 playerRock.addEventListener('click', () => {
 	playerChoice.setAttribute('src', './assets/rock.png');
 	let results = round('rock', getComputerChoice());
 	addScore(results);
 	setTimeout(clearBoard, 2000);
+	checkWin();
 });
 
 playerPaper.addEventListener('click', () => {
@@ -102,6 +122,7 @@ playerPaper.addEventListener('click', () => {
 	let results = round('paper', getComputerChoice());
 	addScore(results);
 	setTimeout(clearBoard, 2000);
+	checkWin();
 });
 
 playerScissors.addEventListener('click', () => {
@@ -109,4 +130,5 @@ playerScissors.addEventListener('click', () => {
 	let results = round('scissors', getComputerChoice());
 	addScore(results);
 	setTimeout(clearBoard, 2000);
+	checkWin();
 });
